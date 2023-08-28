e36d278e2f12a1cbc7503615bf228287409f8caa
baskara restu <baskararw10@gmail.com>
2023-08-28 16:01:51 +0700

    Create Header and Intro

---
+++ src/components/Intro.tsx
@@ -0,0 +1,91 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import profile from "@@/public/foto_saya2.jpg";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+
+function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0 flex flex-col">
+      <div className="flex justify-center items-center">
+        <div className="relative">
+          <motion.div
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src={profile}
+              alt="my-profile"
+              width="192"
+              height="192"
+              priority={true}
+              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="text-4xl absolute bottom-0 right-0"
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-4xl font-medium !leading-[1.5] sm:text-2xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Baskara.</span> I'm a
+        <span className="font-bold"> junior full-stack web developer</span>
+        <span>and welcome to my personal website </span>
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2.5 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a
+          className="group bg-white  px-7 py-4 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/cv_baskara.pdf"
+          download={true}
+        >
+          Download CV
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <a className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10">
+          <BsLinkedin />
+        </a>
+        <a
+          className="bg-white text-gray-700 text-[1.35rem] p-4 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/baskararestu"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
+
+export default Intro;
